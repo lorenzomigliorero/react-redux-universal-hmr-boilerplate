1bfc44eac399f09e5df497a75ca2c7060919c0fd
lorenzomigliorero <lorenzomigliorero@gmail.com>
2017-07-10 17:54:17 +0200

    remove redux-simple-router from deps

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { routeReducer as routing } from 'redux-simple-router';
 import { reducer as images } from 'state/Images/';
 import { reducer as players } from 'state/Players/';
 
 export default combineReducers({
 	images,
-	players,
-	routing
+	players
 });
